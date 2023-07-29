b199a059fd776f127d9673ff0a338ae2d9fa3d0e
Alex Pritchard <hi@apritc.com>
2023-07-29 13:34:13 +0100

    GM Singleton

--- icojam-2023/Assets/Scripts/GameManager.cs
+++ icojam-2023/Assets/Scripts/GameManager.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private static GameManager instance;
+    public static GameManager Instance { get { return instance; } }
 
-    // Update is called once per frame
-    void Update()
+    // Start is called before the first frame update
+    void Awake()
     {
-        
+        instance = this;
+        DontDestroyOnLoad(gameObject);
     }
 }
