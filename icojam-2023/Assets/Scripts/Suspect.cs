548068da17e3134d2d58028e020e88ce5666a3de
Alex Pritchard <hi@apritc.com>
2023-07-29 17:36:27 +0100

    Add silly little guy animations

--- icojam-2023/Assets/Scripts/Suspect.cs
+++ icojam-2023/Assets/Scripts/Suspect.cs
@@ -1,10 +1,27 @@
 using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class Suspect : MonoBehaviour
+public class Suspect : MonoBehaviour, IPointerEnterHandler
 {
     public int lineNumber;
     public int currentPosition;
+
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Base Layer.Default"))
+        {
+            animator.Play("Select");
+        }
+    }
 }
